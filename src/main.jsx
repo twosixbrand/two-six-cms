2c7df91639a1f141c240b241f377a78c681d9ce7
Juliana Manrique <julymanrij@gmail.com>
2025-11-21 17:30:04 -0500

    Se borra archivo index.css para evitar conflictos de estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+
 import App from '/src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
