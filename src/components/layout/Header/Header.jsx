cc3de0d84a4b95f6836b775b479d9f7ad0f855e6
Jorge A Manrique <jamanrique@gmail.com>
2025-11-29 13:04:09 -0500

    Se centra el titulo en la pagina, el logo a la derecha de la pagina

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -8,9 +8,10 @@ const Header = ({ toggleMenu }) => {
     <header className="app-header">
       <div className="header-container">
         <button className="menu-toggle-header" onClick={toggleMenu}>☰</button>
-        
-        <NavLink to="/" className="header-logo-link">
+        <NavLink to="/" className="header-title-link">
           <h1 className="header-title">Two Six Cms</h1>
+        </NavLink>
+        <NavLink to="/" className="header-logo-link">
           <img src={logoUrl} alt="Logo" className="header-logo-image" />
         </NavLink>
       </div>
